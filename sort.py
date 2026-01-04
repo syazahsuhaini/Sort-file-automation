42b070cd63dd915d9127d1b2348210d85998054e
syazahsuhaini <syazasuhaini@gmail.com>
2026-01-04 22:49:56 +0800

    version 1.0 - upgrade move files based on their extensions function, add rename files function

--- sort.py
+++ sort.py
@@ -2,14 +2,15 @@
 import sys
 from shutil import move
 
-from data_path import root_dir
+from data_path import *
 from category import categories
 
 def sort_files():
 
     root_dir.mkdir(parents=True, exist_ok=True)
+    timesheet_dir.mkdir(parents=True, exist_ok=True)
 
-    print(f'\n📌 Scanning directory: {root_dir}')
+    print(f'\n📌 Scanning directory for files: {root_dir}')
     # appending files in Downloads into list
     files = []
 
@@ -26,7 +27,7 @@ def sort_files():
         # move the files in list into respective folders
         for file in files:
             for category in categories.values():
-                if file.endswith(tuple(category['Extensions'])):
+                if file.lower().endswith(tuple(category['Extensions'])):
                     category['Path'].mkdir(exist_ok=True)
 
                     initial_path = root_dir / file
@@ -38,11 +39,36 @@ def sort_files():
                     move(initial_path, destination_path)
                     break
 
+    print(f'\n📌 Scanning directory timesheets: {documents_dir}')
+    files = []
+
+    for f in documents_dir.iterdir():
+        if f.name.startswith('timesheet_'):
+            print(f'Found: {f.name}')
+            files.append(f.name)
+
+    files_count = len(files)
+    print(f'Timesheets found: {files_count}')
+
+    if files_count > 1:
+        print(f'\n📌 Move files into Timesheet')
+        for file in files:
+            initial_path = documents_dir / file
+            destination_path = timesheet_dir / file
+
+            print(f'Moving: {file}')
+            print(f' → Into folder: {destination_path.name}')
+
+            move(initial_path, destination_path)
+
+
     return
 
+"""
 if __name__ == '__main__':
     if len(sys.argv) != 1:
         print('Usage: python sort.py')
         sys.exit(1)
 
     sort_files()
+"""
